b8770107b4ec532db5afad6799743900bd76eb3e
Winston Smith <winstonsmithcodes@gmail.com>
2019-01-04 14:12:25 +0700

    add responsive nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,12 +4,12 @@ import './style.css';
 const NavBar = props => (
   <div>
     <header>
-      <h2><a href="#" class="home">winstonCodes</a></h2>
-      <nav>
-        <li><a href="#" class="about">About</a></li>
-        <li><a href="#" class="portfolio">Portfolio</a></li>
-        <li><a href="#" class="blog">Blog</a></li>
-        <li><a href="#" class="contact">Contact</a></li>
+      <h2><a href="#" class="home grow">winstonCodes</a></h2>
+      <nav class="nav-container">
+        <li><a href="#" class="about grow">About</a></li>
+        <li><a href="#" class="portfolio grow">Portfolio</a></li>
+        <li><a href="#" class="blog grow">Blog</a></li>
+        <li><a href="#" class="contact grow">Contact</a></li>
       </nav>
     </header>
   </div>
