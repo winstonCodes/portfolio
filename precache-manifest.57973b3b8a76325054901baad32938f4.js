c5a4442b4228585b208e0f265c514e70d49e30ba
Winston Smith <winstonsmithcodes@gmail.com>
2019-01-04 12:51:47 +0700

    Updates

---
+++ precache-manifest.57973b3b8a76325054901baad32938f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cf4f73a3aadc0cafbc56",
+    "url": "/static/js/main.cf4f73a3.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "cf4f73a3aadc0cafbc56",
+    "url": "/static/css/main.952a39e2.chunk.css"
+  },
+  {
+    "revision": "bc94cff115310086c79f705508d1415b",
+    "url": "/index.html"
+  }
+];
