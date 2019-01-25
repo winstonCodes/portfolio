55864caf2abfa3ef29f968efe664f7d2f46a3117
Winston Smith <winstonsmithcodes@gmail.com>
2019-01-25 18:06:15 +0700

    adjust navbar, add to about page, add additonal projects to projects file

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -5,7 +5,8 @@ const AboutPage = props => (
   <div>
 
     <section className="hero">
-      <h1>About Page</h1>
+      <h1>About Me:</h1>
+      <p>I'm a self-taught web developer from the US, currently living in Thailand. I have a daughter whom I homeschool, and a wife who teaches English. My previous professions include accounting (4 years) and calculus tutoring (6 years).</p>
     </section>
 
 
--- src/pages/PortfolioPage/components/Projects.js
+++ src/pages/PortfolioPage/components/Projects.js
@@ -1,19 +1,35 @@
 export default [
   {
     title:"Landing Page w/ Multiple Themes",
-    subtitle:"Written in React, HTML, CSS/Sass",
+    subtitle:"React, HTML, CSS/Sass",
     description:"Demo landing page showing responsive design. Multiple themes on click",
-    image: require("../images/desktop-preview.jpg"),
+    image: require("../images/fylo.jpg"),
     demo:"https://winstoncodes.github.io/fylo-react/",
     code:"https://github.com/winstonCodes/fylo-react"
   },
   {
-    title:"Landing Page w/ Multiple Themes",
-    subtitle:"Written in React, HTML, CSS/Sass",
-    description:"Demo landing page showing responsive design. Multiple themes on click",
-    image: require("../images/desktop-preview.jpg"),
-    demo:"https://winstoncodes.github.io/fylo-react/",
-    code:"https://github.com/winstonCodes/fylo-react"
+    title:"Decorative Landing Page",
+    subtitle:"HTML, CSS/Sass",
+    description:"Demo landing page showing responsive design. HTML, CSS, and decorative background coded by myself. Design specs and image assets courtesy of Frontendmentor.io",
+    image: require("../images/huddle-decorative.jpg"),
+    demo:"https://winstoncodes.github.io/frontendmentor-huddle-decorative-landing-page/",
+    code:"https://github.com/winstonCodes/frontendmentor-huddle-decorative-landing-page"
+  },
+  {
+    title:"Landing Page w/ Alternating Sections",
+    subtitle:"HTML, CSS/Sass",
+    description:"Demo landing page showing responsive design. HTML and CSS coded by myself. Design specs and image assets courtesy of Frontendmentor.io",
+    image: require("../images/huddle-alternating.jpg"),
+    demo:"https://winstoncodes.github.io/alternating-sections-huddle/",
+    code:"https://github.com/winstonCodes/alternating-sections-huddle"
+  },
+  {
+    title:"Simple Landing Page",
+    subtitle:"HTML, CSS/Sass",
+    description:"Demo landing page showing responsive design. HTML and CSS coded by myself. Design specs and image assets courtesy of Frontendmentor.io",
+    image: require("../images/simple-landing.jpg"),
+    demo:"https://winstoncodes.github.io/frontendmentor-huddle-landing-page/",
+    code:"https://github.com/winstonCodes/frontendmentor-huddle-landing-page "
   },
 
 
