79ef6ab4d843c3392d4b8d4862b48ed8ce1dafe9
Winston Smith <winstonsmithcodes@gmail.com>
2019-01-26 16:34:48 +0700

    deploy tweaks

---
+++ build/precache-manifest.a5631d032c7a9679ac30c976ce1098bc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0640cba5b67efad24c4b9a62dc77e98",
+    "url": "/portfolio/static/media/calculator.f0640cba.jpg"
+  },
+  {
+    "revision": "06b1cf5f0c6591564c78",
+    "url": "/portfolio/static/css/main.dc67e476.chunk.css"
+  },
+  {
+    "revision": "8e5e4db60090f24a41be",
+    "url": "/portfolio/static/js/1.8e5e4db6.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "72e78620a727c18a3e6f5317b462e531",
+    "url": "/portfolio/static/media/fylo-themes.72e78620.jpg"
+  },
+  {
+    "revision": "6c89537ed863e625e523d0b9c77ecc38",
+    "url": "/portfolio/static/media/portfolio.6c89537e.jpg"
+  },
+  {
+    "revision": "06b1cf5f0c6591564c78",
+    "url": "/portfolio/static/js/main.06b1cf5f.chunk.js"
+  },
+  {
+    "revision": "6b5676caa8c9c87588d93b9c9a6c1830",
+    "url": "/portfolio/static/media/fylo-day-night.6b5676ca.jpg"
+  },
+  {
+    "revision": "f08fe097d9bbda62650c5c064d08a719",
+    "url": "/portfolio/static/media/huddle-decorative.f08fe097.jpg"
+  },
+  {
+    "revision": "7206a06037cdc24fd4007d147dd1510c",
+    "url": "/portfolio/static/media/huddle-alternating.7206a060.jpg"
+  },
+  {
+    "revision": "6963cac11e151527caa5c1a11b9fc8b0",
+    "url": "/portfolio/static/media/simple-landing.6963cac1.jpg"
+  },
+  {
+    "revision": "c15a182a0f0130e5cfc186dcad0f8efa",
+    "url": "/portfolio/index.html"
+  }
+];
